c4af441faaf68ad8947dbb9cb6085cdd1da01b67
Steve Alsop <me@stevenalsop.com>
2020-11-11 14:52:25 +0000

    set app insights to always deploy

--- application_insights.tf
+++ application_insights.tf
@@ -1,9 +1,8 @@
 resource "azurerm_application_insights" "main" {
-  count               = var.vmseries.no_of_instances > 0 ? 1 : 0
+  count               = 1
   name                = "ngfw-appinsights"
   location            = var.resource_location
   resource_group_name = var.resource_group_name
   application_type    = "web"
   depends_on          = [azurerm_resource_group.main]
-
 }
