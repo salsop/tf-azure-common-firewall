9b2584352a2e3638a946192fd31a06abca34f21d
Steve Alsop <me@stevenalsop.com>
2021-05-18 12:24:10 +0100

    fixed issues to make it available as module for deployments

--- README.md
+++ README.md
@@ -35,3 +35,46 @@ vmseries-flex  paloaltonetworks  byol     paloaltonetworks:vmseries-flex:byol:9.
 vmseries-flex  paloaltonetworks  byol     paloaltonetworks:vmseries-flex:byol:9.1.3      9.1.3
 vmseries-flex  paloaltonetworks  byol     paloaltonetworks:vmseries-flex:byol:9.1.6      9.1.6
 ```
+
+
+## Module Use:
+You can also use this Repo as a basic module for deployment:
+
+```terraform
+module "panos-hub" {
+  source = "github.com/salsop/tf-azure-common-firewall"
+
+//  Azure Configuration
+  resource_group_name = "panos-hub"
+  resource_location   = "westeurope"
+    
+  virtual_network_name = "vnet"
+  virtual_network_cidr = "10.1.0.0/16"
+
+//  Panorama Configuration
+  panorama = {
+    primary     = "panorama.paloaltonetworks.com"
+    secondary   = ""
+    vm_auth_key = "44******25"
+    apikey      = "LU***=="
+  }
+
+//  VM-Series
+  vmseries = {
+    no_of_instances = 2
+    version         = "10.0.4"
+    license         = "bundle2"
+    offer           = "vmseries-flex"
+    instance_size = "Standard_DS3_v2"
+    authcodes = ""
+
+    admin_username = "pandemo"
+    admin_password = "Pal0Alto!"
+
+    public_management = true
+  }
+
+  // CSP Self Registration
+  csp_pin_id    = ""
+  csp_pin_value = ""
+```
--- bootstrap.tf
+++ bootstrap.tf
@@ -23,12 +23,6 @@ resource "azurerm_storage_share_directory" "plugins" {
     command = "az storage file upload-batch --account-name ${azurerm_storage_account.bootstrap.name} --account-key ${azurerm_storage_account.bootstrap.primary_access_key} --destination ${azurerm_storage_share.bootstrap.name}/plugins  --source bootstrap_files/plugins"
   }
 
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "az storage file delete-batch --account-name ${self.storage_account_name}  --source bootstrap/plugins"
-    on_failure = continue
-  }
-
 }
 
 resource "azurerm_storage_share_directory" "software" {
@@ -40,24 +34,12 @@ resource "azurerm_storage_share_directory" "software" {
     command = "az storage file upload-batch --account-name ${azurerm_storage_account.bootstrap.name} --account-key ${azurerm_storage_account.bootstrap.primary_access_key} --destination ${azurerm_storage_share.bootstrap.name}/software  --source bootstrap_files/software"
   }
 
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "az storage file delete-batch --account-name ${self.storage_account_name} --source bootstrap/software"
-    on_failure = continue
-  }
-
 }
 
 resource "azurerm_storage_share_directory" "license" {
   name                 = "license"
   share_name           = azurerm_storage_share.bootstrap.name
   storage_account_name = azurerm_storage_account.bootstrap.name
-
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "az storage file delete-batch --account-name ${self.storage_account_name} --source bootstrap/license"
-    on_failure = continue
-  }
 
 }
 
@@ -69,10 +51,6 @@ resource "azurerm_storage_share_directory" "content" {
   provisioner "local-exec" {
     command = "az storage file upload-batch --account-name ${azurerm_storage_account.bootstrap.name} --account-key ${azurerm_storage_account.bootstrap.primary_access_key} --destination ${azurerm_storage_share.bootstrap.name}/content  --source bootstrap_files/content"
   }
-
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
 
   provisioner "local-exec" {
     when       = destroy
@@ -88,13 +66,7 @@ resource "azurerm_storage_share_directory" "config" {
   storage_account_name = azurerm_storage_account.bootstrap.name
 
   provisioner "local-exec" {
-    command = "az storage file upload-batch --account-name ${azurerm_storage_account.bootstrap.name} --account-key ${azurerm_storage_account.bootstrap.primary_access_key} --destination ${azurerm_storage_share.bootstrap.name}/config  --source tmp/config"
-  }
-
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "az storage file delete-batch --account-name ${self.storage_account_name} --source bootstrap/config"
-    on_failure = continue
+    command = "az storage file upload-batch --account-name ${azurerm_storage_account.bootstrap.name} --account-key ${azurerm_storage_account.bootstrap.primary_access_key} --destination ${azurerm_storage_share.bootstrap.name}/config  --source ./tmp/${random_string.name.result}/config"
   }
 
   depends_on = [local_file.initcfg_txt]
@@ -102,7 +74,7 @@ resource "azurerm_storage_share_directory" "config" {
 
 resource "null_resource" "license" {
   provisioner "local-exec" {
-    command = "az storage file upload-batch --account-name ${azurerm_storage_account.bootstrap.name} --account-key ${azurerm_storage_account.bootstrap.primary_access_key} --destination ${azurerm_storage_share.bootstrap.name}/license  --source tmp/license"
+    command = "az storage file upload-batch --account-name ${azurerm_storage_account.bootstrap.name} --account-key ${azurerm_storage_account.bootstrap.primary_access_key} --destination ${azurerm_storage_share.bootstrap.name}/license  --source ./tmp/${random_string.name.result}/license"
   }
 
   triggers = {
@@ -113,20 +85,19 @@ resource "null_resource" "license" {
 }
 
 data "template_file" "authcodes" {
-  template = file("template_files/authcodes.template")
+  template = file("${path.module}/template_files/authcodes.template")
   vars = {
     authcodes = var.vmseries.authcodes
   }
 }
 
 resource "local_file" "authcodes" {
-  filename = "${path.module}/tmp/license/authcodes"
+  filename = "./tmp/${random_string.name.result}/license/authcodes"
   content  = data.template_file.authcodes.rendered
 }
 
-
 data "template_file" "initcfg_txt" {
-  template = file("template_files/init-cfg.txt.template")
+  template = file("${path.module}/template_files/init-cfg.txt.template")
   vars = {
     panorama_server1 = var.panorama.primary
     panorama_server2 = var.panorama.secondary
@@ -138,8 +109,7 @@ data "template_file" "initcfg_txt" {
   }
 }
 
-
 resource "local_file" "initcfg_txt" {
-  filename = "${path.module}/tmp/config/init-cfg.txt"
+  filename = "./tmp/${random_string.name.result}/config/init-cfg.txt"
   content  = data.template_file.initcfg_txt.rendered
 }
